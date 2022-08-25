1d32709ae92712ed946604abe48c535d573e1697
Marco Rosario Barone <marcorbarone@gmail.com>
2022-08-25 15:11:16 +0200

    route web.php ordered

--- routes/web.php
+++ routes/web.php
@@ -16,32 +16,42 @@ use App\Http\Controllers\RevisorController;
 |
 */
 
-Route::get('/',[PublicController::class,'welcome'])->name('welcome');
 
 
 
-Route::get('/category/{category}',[PublicController::class,'categoryShow'])->name('categoryShow');
-
-Route::get('/index',[PublicController::class,'index'])->name('index');
-
-Route::get('/user/profile', [PublicController::class, 'profileView'])->name('profileView');
 
 
 
-Route::get('/announcement/new',[AnnouncementController::class,'createAnnouncement'])->middleware('auth')->name('createAnnouncement');
 
-Route::get('/announcement/detail/{announcement}',[AnnouncementController::class,'announcementShow'])->name('announcementShow');
-
-/* route revisione */
-Route::patch('/announcement/accept/{announcement}', [RevisorController::class, 'acceptAnnouncement'])->middleware('isRevisor')->name('acceptAnnouncement');
-Route::patch('/announcement/reject/{announcement}', [RevisorController::class, 'rejectAnnouncement'])->middleware('isRevisor')->name('rejectAnnouncement');
-Route::get('/announcement/review', [RevisorController::class, 'rewiewAnnouncement'])->middleware('isRevisor')->name('rewiewAnnouncements');
-Route::get('/revisor/home', [RevisorController::class, 'indexRevisor'])->middleware('isRevisor')->name('indexRevisor');
-
-// Route work like revisor
-Route::get('/request/becomerevisor',[RevisorController::class,'becomeRevisor'])->middleware('auth')->name('becomeRevisor');
-Route::get('/request/makerevisor/{user}',[RevisorController::class,'makeRevisor'])->middleware('auth')->name('makeRevisor');
 
 /* rotte ricerca */
-Route::get('/announcements/research', [PublicController::class, 'searchAnnouncements'])->name('searchAnnouncements');
 
+Route::controller(PublicController::class)->group(
+        function(){
+                
+                Route::get('/', 'welcome')->name('welcome');
+                Route::get('/category/{category}','categoryShow')->name('categoryShow');
+                Route::get('/index','index')->name('index');
+                Route::get('/user/profile', 'profileView')->name('profileView');
+                Route::get('/announcements/research', 'searchAnnouncements')->name('searchAnnouncements');
+        }
+);
+
+Route::controller(AnnouncementController::class)->group(
+        function(){
+                Route::get('/announcement/new','createAnnouncement')->middleware('auth')->name('createAnnouncement');
+                Route::get('/announcement/detail/{announcement}', 'announcementShow')->name('announcementShow');
+        }
+);
+
+Route::controller(RevisorController::class)->group(
+        function(){
+                Route::patch('/announcement/accept/{announcement}', 'acceptAnnouncement')->middleware('isRevisor')->name('acceptAnnouncement');
+                Route::patch('/announcement/reject/{announcement}', 'rejectAnnouncement')->middleware('isRevisor')->name('rejectAnnouncement');
+                Route::get('/announcement/review', 'rewiewAnnouncement')->middleware('isRevisor')->name('rewiewAnnouncements');
+                Route::get('/revisor/home', 'indexRevisor')->middleware('isRevisor')->name('indexRevisor');
+                // Route work like revisor
+                Route::get('/request/becomerevisor', 'becomeRevisor')->middleware('auth')->name('becomeRevisor');
+                Route::get('/request/makerevisor/{user}','makeRevisor')->middleware('auth')->name('makeRevisor');
+        }
+);
