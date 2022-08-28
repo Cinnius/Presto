244ab962a1c9faf35106c20dd3ba28849c017961
Alessio Saccà <alessio.sacca95@hotmail.it>
2022-08-28 02:58:18 +0200

    Created last announcement carousel and small fix

--- resources/js/carousel.js
+++ resources/js/carousel.js
@@ -1,17 +1,33 @@
-var swiper = new Swiper(".mySwiper", {
-    direction: "vertical",
-    spaceBetween: 10,
-    slidesPerView: 8,
-    freeMode: true,
-    watchSlidesProgress: true,
-  });
-var swiper2 = new Swiper(".mySwiper2", {
-    spaceBetween: 10,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    thumbs: {
-      swiper: swiper,
-    },
+let swiper = new Swiper(".mySwiper", {
+  direction: "vertical",
+  spaceBetween: 10,
+  slidesPerView: 8,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+let swiper2 = new Swiper(".mySwiper2", {
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: swiper,
+  },
+});
+let swiper3 = new Swiper(".mySwiper3", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
