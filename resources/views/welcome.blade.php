3bfe0cecaf2ff1c885854e7053a7ff6817f97a8f
Carlo Bellantone <carlobellantone23@gmail.com>
2022-08-29 18:59:21 +0200

    small adjustement

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -34,7 +34,7 @@
         <div class="row justify-content-between align-items-center pt-5 pt-md-2 h-header-custom">
             <!-- logo -->
             <div class="col-12 col-md-7 col-xxl-6 d-flex flex-column align-items-center">
-                <h1 class="dark-text fw-bolder display-4"> {{__('ui.slogan')}} </h1>
+                <h1 class="dark-text fw-bolder display-4 fs-1 ps-3 ms-5"> {{__('ui.slogan')}} </h1>
                 <div class="col-11 col-xl-10 d-flex justify-content-center justify-content-lg-end ms-3 ms-xl-5">
                     <img class="logo-custom img-fluid ps-5" src="image/logo1.png" alt="word Presto">
                 </div>
@@ -164,7 +164,7 @@
     {{-- Section our team, deliver, ... --}}
     <section class="container-fluid mt-5 pt-5" id="ourteam">
         <h3 class="text-center fw-semibold">{{__('ui.our_Teams')}}</h3>
-        <p class="text-center">{{__('ui.out_Teams_Text')}}</p>
+        <p class="text-center">{{__('ui.our_Teams_Text')}}</p>
         <div class="row justify-content-center">
             <div class="col-12 col-md-3 py-4 d-flex justify-content-center">
                 <div class="card cardFlipPersonalize">
