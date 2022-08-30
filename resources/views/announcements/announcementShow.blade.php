cd5d397223bf7af12cfd9d104bb48076025790f8
Alessio Saccà <alessio.sacca95@hotmail.it>
2022-08-30 11:57:23 +0200

    fix

--- resources/views/announcements/announcementShow.blade.php
+++ resources/views/announcements/announcementShow.blade.php
@@ -1,5 +1,5 @@
 <x-layout>
-    <div class="container-fluid mt-5">
+    <div class="container-fluid margin-top">
         <div class="row justify-content-center">
             {{-- Carousel Thumbnail --}}
             <div class="col-1">
@@ -20,7 +20,7 @@
                 </div>
             </div>
             {{-- Carousel View --}}
-            <div class="col-4">
+            <div class="col-5">
                 <div style="--swiper-navigation-color: #fff; --swiper-pagination-color: #fff" class="swiper mySwiper2">
                     <div class="swiper-wrapper">
                         @if ($announcement->images->isNotEmpty())
@@ -40,8 +40,8 @@
                 </div>
             </div>
             {{-- Description --}}
-            <div class="col-4">
-                <div class="gradient-custom p-3 rounded">
+            <div class="col-5">
+                <div class="gradient-custom border border-dark shadow p-3 rounded">
                     <h2>{{$announcement->title}}</h2>
                     <h6 class="ms-4 my-2">
                         <span class="text-decoration-none text-dark shadow white-bg px-2 rounded me-3">{{$announcement->category->name}}</span>
@@ -50,19 +50,15 @@
                     <h3>€ {{$announcement->price}}</h3>
                     <p>{{$announcement->body}}</p>
                     <hr class="mt-0 mb-3">
-                </div>
-            </div>
             {{-- Other Information --}}
-            <div class="col-2">
-                <div>
-                    <p class="mt-3 px-2 py-1">{{__('ui.announcements_Delivery')}}</p>
-                </div>
-                <div class="mt-3 ms-3 light-bg px-3 py-2">
                     <h4>{{__('ui.announcements_Info')}}</h4>
                     <div>
-                        <p class="my-1">{{__('ui.nnouncement_Created')}} {{$announcement->created_at->format('d/m/Y')}}</p>
-                        <p class="my-1">{{__('ui.nnouncement_Updated')}} {{$announcement->updated_at->format('d/m/Y')}}</p>
-                        <p class="my-1">{{__('ui.nnouncement_Seller')}} {{$announcement->user->name ?? ''}}</p>
+                        <p class="mt-3 px-2 py-1">{{__('ui.announcements_Delivery')}}</p>
+                    </div>
+                    <div>
+                        <p class="my-1">{{__('ui.Announcement_Created')}} {{$announcement->created_at->format('d/m/Y')}}</p>
+                        <p class="my-1">{{__('ui.Announcement_Updated')}} {{$announcement->updated_at->format('d/m/Y')}}</p>
+                        <p class="my-1">{{__('ui.Announcement_Seller')}} {{$announcement->user->name ?? ''}}</p>
                     </div>
                 </div>
             </div>
