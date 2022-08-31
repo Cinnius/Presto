bf105e93df4bd5261d3821fb7778d3466d165041
Alessio Saccà <alessio.sacca95@hotmail.it>
2022-08-31 15:59:40 +0200

    created comment model and relations with user

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Models\Comment;
 use App\Models\Announcement;
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
@@ -45,5 +46,9 @@ class User extends Authenticatable
 
     public function announcements(){
         return $this -> hasMany(Announcement::class);
+    }
+
+    public function comments(){
+        return $this -> hasMany(Comment::class);
     }
 }
