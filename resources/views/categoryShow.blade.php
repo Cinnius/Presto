42fa09d9772bcb5e7eceafde35d7d609bb256780
Marco Rosario Barone <marcorbarone@gmail.com>
2022-09-03 12:55:34 +0200

    small adjustament welcome and showCategory

--- resources/views/categoryShow.blade.php
+++ resources/views/categoryShow.blade.php
@@ -2,7 +2,7 @@
     <x-slot name="title">Annunci inseriti</x-slot>
     {{-- category zone --}}
     <div class="container-fluid pt-2 py-5 mt-5">
-        <div class="row visibilityHide justify-content-around">
+        <div class="row visibilityHide justify-content-around mt-3">
             @foreach ($categories as $category)
                 <div class="col-1 my-3  d-flex justify-content-center">
                     <a class="link-costum"href="{{ route('categoryShow', compact('category')) }}">
